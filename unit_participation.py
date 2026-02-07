e2d933cbf08cced82e03194e0acdaafb6af6ec9f
Gnalen Mara <gnalenmara80@gmail.com>
2026-02-06 20:25:08 -0500

    generate 100 random numbers and compute total, sum, and average

--- unit_participation.py
+++ unit_participation.py
@@ -8,4 +8,9 @@ Date: 02/06/2026
 import random
 # this will create a list of 100 random integers between 1 and 10
 random_list = [random.randint(1, 10) for _ in range(100)]
-# this will output the total 
+# this will output the total number of integers in the list
+print("Total elements:", len(random_list))
+# this will print the sum of all numbers in the list
+print("Sum:", sum(random_list))
+# this will print the average of the numbers in the list
+print("Average:", sum(random_list) / len(random_list))
