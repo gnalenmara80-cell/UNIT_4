011d21eac627f64bfb30e7745c68bbdaaad92ad8
Gnalen Mara <gnalenmara80@gmail.com>
2026-02-06 20:09:37 -0500

    Created a list of random numbers

---
+++ unit_participation.py
@@ -0,0 +1,11 @@
+"""
+Programme name: unit_participation.py
+Author: Gnalen Mara
+Purpose: this programme use list comprehension to create a list of 100 random integers between 1 to 10
+Starter code: None
+Date: 02/06/2026
+"""
+import random
+# this will create a list of 100 random integers between 1 and 10
+random_list = [random.randint(1, 10) for _ in range(100)]
+# this will output the total 
