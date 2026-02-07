268de1854fb958edb0a9839790a9c3b3aeef6271
Gnalen Mara <gnalenmara80@gmail.com>
2026-02-06 21:18:12 -0500

    changer random_list to number_list

--- unit_participation.py
+++ unit_participation.py
@@ -7,10 +7,10 @@ Date: 02/06/2026
 """
 import random
 # this will create a list of 100 random integers between 1 and 10
-random_list = [random.randint(1, 10) for _ in range(100)]
+number_list = [random.randint(1, 10) for _ in range(100)]
 # this will output the total number of integers in the list
-print("Total elements:", len(random_list))
+print("Total elements:", len(number_list))
 # this will print the sum of all numbers in the list
-print("Sum:", sum(random_list))
+print("Sum:", sum(number_list))
 # this will print the average of the numbers in the list
-print("Average:", sum(random_list) / len(random_list))
+print("Average:", sum(number_list) / len(number_list))
